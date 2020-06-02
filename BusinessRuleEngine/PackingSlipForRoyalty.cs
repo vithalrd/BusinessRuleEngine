ef2d1cd7f063ea0bb98ce0d5af3b6c39da0913e0
vithalrd <vithalrd@gmail.com>
2020-06-02 23:27:20 +0530

    Generate packing ship for royalty dept code added

---
+++ BusinessRuleEngine/PackingSlipForRoyalty.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessRuleEngine
+{
+    public class PackingSlipForRoyalty : IAction
+    {
+        private PaymentDetailsDto paymentDetailsDto;
+
+        public PackingSlipForRoyalty(PaymentDetailsDto paymentDetailsDto)
+        {
+            this.paymentDetailsDto = paymentDetailsDto;
+        }
+        public string DoProcess()
+        {
+            string result = string.Format("Generated duplicate packing slip for Royalty Department of Product Id {0}",
+                paymentDetailsDto.ProductDto.ProductId);
+            Console.WriteLine(result);
+            return result;
+        }
+    }
+}
