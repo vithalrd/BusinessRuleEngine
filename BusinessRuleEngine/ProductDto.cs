fca8b6bffff8fdc3ee4e1460480dcb1b75a3996a
vithalrd <vithalrd@gmail.com>
2020-06-02 19:04:20 +0530

    Product Dto and Enum added

---
+++ BusinessRuleEngine/ProductDto.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessRuleEngine
+{
+    /// <summary>
+    /// Payment Dto, which has all dto related to processing
+    /// </summary>
+    public class PaymentDetailsDto
+    {
+        public ProductEnum ProductType { get; set; }
+        public ProductDto ProductDto { get; set; }
+        public CustomerDto CustomerDto { get; set; }
+        public MembershipDto MembershipDto { get; set; }
+        public AgentDto AgentDto { get; set; }
+    }
+
+    /// <summary>
+    /// Product related properties
+    /// </summary>
+    public class ProductDto
+    {
+        public int ProductId { get; set; }
+        public int Price { get; set; }
+    }
+
+    /// <summary>
+    /// Customer related properties
+    /// </summary>
+    public class CustomerDto
+    {
+        public string CustomerName { get; set; }
+        public int CustomerId { get; set; }
+        public string CustomerEmail { get; set; }
+    }
+
+    /// <summary>
+    /// Membership related properties
+    /// </summary>
+    public class MembershipDto 
+    {
+        public int MembershipId { get; set; }
+        public string MembershipName { get; set; }
+    }
+
+    /// <summary>
+    /// Agent related properties
+    /// </summary>
+    public class AgentDto
+    {
+        public int AgentId { get; set; }
+        public string AgentName { get; set; }
+    }
+
+   
+}
---
+++ BusinessRuleEngine/ProductEnum.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessRuleEngine
+{
+    /// <summary>
+    /// Product Enum to identify the type of product
+    /// </summary>
+    public enum ProductEnum
+    {
+        PhysicalProduct = 1,
+        Book = 2,
+        ActivateMembership = 3,
+        UpgradeMembership = 4,
+        Video = 5
+    }
+}
