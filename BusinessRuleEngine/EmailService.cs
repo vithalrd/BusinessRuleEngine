7725af259830d941f55506d6fa3ee4a125e1f028
vithalrd <vithalrd@gmail.com>
2020-06-02 20:24:00 +0530

    Send Email functionality added

---
+++ BusinessRuleEngine/EmailService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessRuleEngine
+{
+    public class EmailService 
+    {
+        private PaymentDetailsDto paymentDetailsDto;
+
+        public EmailService(PaymentDetailsDto paymentDetailsDto)
+        {
+            this.paymentDetailsDto = paymentDetailsDto;
+        }
+        public string DoProcess()
+        {
+            string result = string.Format("Email has been sent to Email Id {0}",
+              paymentDetailsDto.CustomerDto.CustomerEmail);
+            Console.WriteLine(result);
+            return result;
+        }
+    }
+}
