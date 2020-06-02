9477ded61c329cde29c47f09ce9d8e2482425789
vithalrd <vithalrd@gmail.com>
2020-06-02 20:26:17 +0530

    Unit test got missed in last commit. Added unit test of Send Email and committed

---
+++ BusinessRuleEngineUnitTest/SendEmailTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using BusinessRuleEngine;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BusinessRuleEngineUnitTest
+{
+    [TestClass]
+    public class SendEmailTest
+    {
+        [TestMethod]
+        public void SendEmail_Test()
+        {
+            PaymentDetailsDto paymentDetailsDto = new PaymentDetailsDto();
+            paymentDetailsDto.CustomerDto = new CustomerDto
+            {
+                CustomerEmail = "vithalrd@gmail.com",
+                CustomerId = 120,
+                CustomerName = "Vithal Deshpande"
+            };
+
+            EmailService emailService = new EmailService(paymentDetailsDto);
+            string packingSlipResult = emailService.DoProcess();
+            Assert.IsNotNull(emailService);
+        }
+    }
+}
