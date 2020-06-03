e2c8da6eb919d0e492e5bef5e349f120e97a52fe
vithalrd <vithalrd@gmail.com>
2020-06-03 13:27:44 +0530

    Payment process service added. Payment Process integration test case added to test end to end payment process actions

---
+++ BusinessRuleEngine/PaymentProcessingService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessRuleEngine
+{
+    /// <summary>
+    /// This services returns list of actions to executed on product payment
+    /// </summary>
+    public class PaymentProcessingService
+    {
+        List<IAction> actions = new List<IAction>();
+
+        public PaymentProcessingService(PaymentDetailsDto productDto)
+        {
+            actions = ActionFactory.CreateActions(productDto);
+        }
+
+        public void ProcessPayment()
+        {
+            foreach (var action in actions)
+            {
+                action.DoProcess();
+            }
+        }
+    }
+}
--- BusinessRuleEngineUnitTest/PaymentProcessIntegrationTest.cs
+++ BusinessRuleEngineUnitTest/PaymentProcessIntegrationTest.cs
@@ -1,14 +1,31 @@
 ﻿using System;
+using BusinessRuleEngine;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace BusinessRuleEngineUnitTest
 {
     [TestClass]
-    public class UnitTest1
+    public class IntegrationTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void PaymentProcessIntegrationTest()
         {
+            PaymentDetailsDto paymentDetailsDto = new PaymentDetailsDto();
+            paymentDetailsDto.ProductType = ProductEnum.PhysicalProduct;
+            paymentDetailsDto.ProductDto = new ProductDto
+            {
+                Price = 1200,
+                ProductId = 1
+            };
+
+            paymentDetailsDto.AgentDto = new AgentDto
+            {
+                AgentId = 200,
+                AgentName = "John Miller"
+            };
+
+            PaymentProcessingService paymentProcessingService = new PaymentProcessingService(paymentDetailsDto);
+            paymentProcessingService.ProcessPayment();
         }
     }
 }
