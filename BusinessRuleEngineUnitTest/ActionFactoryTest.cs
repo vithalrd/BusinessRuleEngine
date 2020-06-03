79a92a68d132a4e550b9b940269e3f4c0c526307
vithalrd <vithalrd@gmail.com>
2020-06-03 13:07:57 +0530

    Action Factory unit test added and action factory code added to return list of action to perform once payment is done for product

---
+++ BusinessRuleEngineUnitTest/ActionFactoryTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using BusinessRuleEngine;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BusinessRuleEngineUnitTest
+{
+    [TestClass]
+    public class ActionFactoryTest
+    {
+        [TestMethod]
+        public void ActionsForPhysicalProduct_Test()
+        {
+            PaymentDetailsDto paymentDetailsDto = new PaymentDetailsDto();
+            paymentDetailsDto.ProductType = ProductEnum.PhysicalProduct;
+            paymentDetailsDto.ProductDto = new ProductDto
+            {
+                Price = 1200,
+                ProductId = 1
+            };
+
+            var actions = ActionFactory.CreateActions(paymentDetailsDto);
+            Assert.IsTrue(actions.Count == 2); // one of packing slip for shipping and agent payment generation
+
+            paymentDetailsDto.AgentDto = new AgentDto
+            {
+                AgentId = 200,
+                AgentName = "John Miller"
+            };
+            foreach (var action in actions)
+            {
+                Assert.IsNotNull(action.DoProcess());
+            }
+
+        }
+    }
+}
