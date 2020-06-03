691b7d6de1c19c7150fe73c1544ddb06df70c321
vithalrd <vithalrd@gmail.com>
2020-06-03 12:57:03 +0530

    Video Payment unit tests and code added

---
+++ BusinessRuleEngineUnitTest/VideoPaymentTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using BusinessRuleEngine;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BusinessRuleEngineUnitTest
+{
+    [TestClass]
+    public class VideoPaymentTest
+    {
+        [TestMethod]
+        public void VideoPayment_Test()
+        {
+            PaymentDetailsDto paymentDetailsDto = new PaymentDetailsDto();
+            paymentDetailsDto.ProductType = ProductEnum.Video;
+            paymentDetailsDto.ProductDto.ProductId = 101;
+            paymentDetailsDto.ProductDto.Price = 200;
+
+
+            IAction videoPayment = new Video(paymentDetailsDto);
+            string result = videoPayment.DoProcess();
+            Assert.IsNotNull(result);
+        }
+
+      
+    }
+}
